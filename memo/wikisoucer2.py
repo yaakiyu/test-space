6303d0f054faabdde1e3b24a2d94d1615f2c87b5
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-10-27 06:49:19 +0000

    upd

--- memo/wikisoucer2.py
+++ memo/wikisoucer2.py
@@ -1,9 +1,20 @@
 # wikisource link formatter
 
-with open("test.txt", encoding="utf-8") as f:
+with open("memo/test.txt", encoding="utf-8") as f:
     data = f.read()
 
-data = "\n".join(data.splitlines()[2050:])
 
-with open("test.txt", "w", encoding="utf-8") as f:
-    f.write(data)
+new_source = data.splitlines()
+
+for count, line in enumerate(new_source):
+    if (
+        line.startswith("||") and new_source[count - 2].startswith("|!")
+        and new_source[count - 1][2:].startswith(
+            ("明治", "大正", "昭和", "平成", "令和")
+        ) and new_source[count - 3] == "|-" and "[[" not in line
+        and count > 2000
+    ):
+        new_source[count] = f"||[[{line[2:]}]]"
+
+with open("memo/test.txt", "w", encoding="utf-8") as f:
+    f.write("\n".join(new_source))
