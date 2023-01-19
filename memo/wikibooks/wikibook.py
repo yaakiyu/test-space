f78e0ddd083ee0e3f339dd986d9820a2512a5686
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2023-01-19 04:32:01 +0000

    メロスは激怒した。

--- memo/wikibooks/wikibook.py
+++ memo/wikibooks/wikibook.py
@@ -6,11 +6,13 @@ with open("memo/wikibooks/wikibook.txt") as f:
     data = f.read()
 
 seiki_hyougen = r"([^a-zA-Z" # アルファベット以外にマッチ
-seiki_hyougen += r"\u3040-\u309F\u30A0-\u30FF\uFF61-\uFF9F、。]" # ひらがなやカタカナ以外にマッチ
+seiki_hyougen += r"\u3040-\u309F\u30A0-\u30FF\uFF61-\uFF9F、。「」]" # ひらがなやカタカナ以外にマッチ
 seiki_hyougen += r"+)" # 繰り返し
 seiki_hyougen += r"（([\u3040-\u309F]+)）" # かっこのなか
 
 data = re.sub(seiki_hyougen, r"{{Ruby|\1|\2}}", data)
+
+data = "<br>\n".join(data.splitlines())
 
 with open("memo/wikibooks/wikibook.txt", mode="w") as f:
     f.write(data)
