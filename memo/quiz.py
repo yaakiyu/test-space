6bec750db7a4ec836509ba427433dfba3769ee7a
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2023-05-17 11:58:07 +0900

    Update quiz.py

--- memo/quiz.py
+++ memo/quiz.py
@@ -1,2 +1,16 @@
 # クイズデータ自動変換システム
-# まだ内容はないようです。
+
+import csv
+
+with open("memo/quiz.csv") as f:
+    m = [i for i in csv.reader(f)]
+
+obj = []
+
+for line in m:
+    if not isinstance(line[0], int):
+        continue
+    obj.append({"question": line[1], "answer": line[2]})
+
+with open("memo/quiz.txt", mode="w") as f:
+    f.write(str(obj))
