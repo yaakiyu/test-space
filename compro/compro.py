1534ae50108a3461037cdd77ccfc28137433e7c0
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2023-12-04 11:55:45 +0900

    Update compro.py

--- compro/compro.py
+++ compro/compro.py
@@ -1,29 +1,20 @@
 # competetive programming
 # 競プロにつかえるもの
-
 INTIN = lambda: int(input())
 def MAPIN(kansu=int):
     return map(kansu, input().split())
-
-# コピペ用
-map(int, input().split())
-
 LISTIN = lambda k=int: list(MAPIN(k))
-
 # sys関連
 import sys
-sys.setrecursionlimit(1000000)
-# sys.set_int_max_str_digits(0)
-
-import sys
+sys.setrecursionlimit(0)
+sys.set_int_max_str_digits(0)
 input = lambda: sys.stdin.readline()[:-1]
-
 # もしpypyなら
-import pypyjit
-pypyjit.set_param('max_unroll_recursion=-1')  # 再帰関数の展開をする
-
-# UnionFindするとき
-from atcoder.dsu import DSU  # 使い方：uf=DSU(頂点数) ; uf.merge(u, v) ; uf.same(u, v) ; uf.leader(u) ; uf.size(u) ; uf.groups()
-
-# SCCするとき
-from stcoder.scc import SCCGraph # 使い方：g=SCCGraph(頂点数) ; g.add_edge(u, v) ; scc=g.scc()
+#import pypyjit; pypyjit.set_param('max_unroll_recursion=-1')  # 再帰関数の展開をする
+#from atcoder.dsu import DSU  # UnionFind　使い方：uf=DSU(頂点数) ; uf.merge(u, v) ; uf.same(u, v) ; uf.leader(u) ; uf.size(u) ; uf.groups()
+#from stcoder.scc import SCCGraph # SCC 使い方：g=SCCGraph(頂点数) ; g.add_edge(u, v) ; scc=g.scc()
+#from collections import deque  # Queue 使い方：q=deque(lis) ; q.popleft(item) ; q.append(item)
+#from collections import Counter # Counter
+#import heapq # Priority Queue 使い方：q=[...] ; heapq.heapify(q) ; heapq.heappush(q, item) ; heapq.heappop(q, item)
+#from itertools import accumulate # 累積和
+#from decimal import Decimal, getcontext; getcontext().prec = 100 # 正確な少数
