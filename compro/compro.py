63f5659d84462e70ac5490805e2a61dc4175128d
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2024-03-29 22:02:15 +0900

    Update compro.py

--- compro/compro.py
+++ compro/compro.py
@@ -9,6 +9,8 @@ if sys.version_info.minor >= 11:
     sys.set_int_max_str_digits(10000000)
 
 input = lambda: sys.stdin.readline()[:-1]
+
+### import関連
 # もしpypyなら
 #import pypyjit; pypyjit.set_param('max_unroll_recursion=-1')  # 再帰関数の展開をする
 #from atcoder.dsu import DSU  # UnionFind　使い方：uf=DSU(頂点数) ; uf.merge(u, v) ; uf.same(u, v) ; uf.leader(u) ; uf.size(u) ; uf.groups()
@@ -19,6 +21,13 @@ input = lambda: sys.stdin.readline()[:-1]
 #from itertools import accumulate # 累積和
 #from bisect import bisect_left, bisect_right # 二分探索
 #from decimal import Decimal, getcontext; getcontext().prec = 100 # 正確な少数
+#from atcoder.segtree import SegTree # セグ木 使い方：tree=SegTree(関数, 単位元, 元リスト) ; tree.set(pos, x) ; tree.prod(left, right + 1) # 閉区間
+
+### 定数関連
+#inf = float("inf")
+#inf = 10**18
+#mod = 998244353
+#mod = 1000000007 # 10**9+7
 
 INTIN = lambda: int(input())
 def MAPIN(kansu=int):
