42f82d7e6efa50f69057f72618407536142d1e5a
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2026-06-25 00:07:16 +0900

    Implement leap year determination in a.c
    Add leap year check and user input for year

--- cpro/report_1/a.c
+++ cpro/report_1/a.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
+#define yes "す\n"
+#define no "ではありません\n"
+
 
 int main() {
-  return 0;
+    int n;
+
+    printf("年を入力: ");
+    scanf("%d", &n);
+
+    printf("%d年はうるう年で", n);
+    if (n % 4 != 0) {
+        printf(no);
+        return 0;
+    }
+    if (n % 400 == 0) {
+        printf(yes);
+        return 0;
+    }
+    if (n % 100 == 0) {
+        printf(no);
+        return 0;
+    }
+    printf(yes);
+
+    return 0;
 }
